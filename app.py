06de569d6e46f682d47a61345436600c31a64997
Devidas Tambe <devitambe102@gmail.com>
2026-08-20 23:51:02 +0530

    Fix formatting issue in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from io import StringIO
 
  
  
-app = Flask(__name__)
+app = Flask(__name__) 
 app.secret_key = "nashik_secret_key" # Flash messages dikhane ke liye zaroori hai
 DB_NAME = "citizen_complaints.db"
 
