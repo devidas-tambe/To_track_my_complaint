9cf955fe208e16a822bb5d1807459596d9d72cac
Devidas Tambe <devitambe102@gmail.com>
2026-09-04 21:18:27 +0530

    Fix formatting of secret_key assignment in app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from io import StringIO
 # @123
  
 app = Flask(__name__)
-app.secret_key = "nashik_secret_key" # Flash messages dikhane ke liye zaroori hai
+app.secret_key = "nashik_secret_key" # Flash messages dikhane ke liye zaroori hai 
 DB_NAME = "citizen_complaints.db" 
 
 # Password ko secure (SHA-256) banane ka function
